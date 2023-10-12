359edfbf9b7e42e50801995171adfea0dc03e75f
unknown <adunolafaneye3@gmail.com>
2023-10-12 15:38:42 +0100

    Latest change

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                 <div class="container">
                   <div class="banner-info-bg text-left">
                     <!-- <p>Better living</p> -->
-                    <h5>Adunola grow rich in their sleep</h5>
+                    <h5> Boss Adunola grow rich in their sleep</h5>
                     <a href="#pay" class="btn btn-white" style="border-radius:20px">Read More</a>
                   </div>
                 </div>
